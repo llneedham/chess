743b4453ba0c44a2b8d583971c26a7e6c3c56d87
llneedham <llneedham@uchicago.edu>
2023-11-26 19:18:52 -0700

    Worked on board, trying to figure out an efficient way to make the layout. Need to learn how to assign the coordinates as well.

--- Board.py
+++
@@ -1,5 +0,0 @@
-# Board class
-#   8 columns (A, B, C, D, E, F, G, H)
-#   8 rows (1, 2, 3, 4, 5, 6, 7, 8)
-
-
--- pieces.py
+++ pieces.py
@@ -1,33 +1,41 @@
 class Piece:
-    location = (column, row)
-    def __init__(self, team):
+    def __init__(self, team, location: Coordinate, status):
         self.team = team
         self.location = location
         self.type = type
+        self.status = status
 
-    def get_location(self, location):
-        return location
+
 class King:
-    def move_king(self, start_location, end_location):
-        self.start_location = get_location
+    def move(self, start_location, end_location):
+        self.start_location = location
+        end_location = input('Please enter the coordinate you want to move to:    ')
+
 
 #   Can move one square in any direction
 #   Can castle with the castle when no pieces are between them and the King is moving for the first time, moving to the midpoint
 #   between the two pieces and swapping sides
 
-# class Queen:
+class Queen:
+    def move(self, start_location, end_location):
+        self.start_location = location
 #   Can move in any direction any number of squares
 
-# class Bishop:
+class Bishop:
+    pass
 #   Can move diagonally any number of squares
 
-# class Rook:
+class Rook:
+    pass
 #   Can move in an L shape in any direction
 
-# class Castle:
+class Castle:
+    pass
 #   Can move horizontally or vertically any number of squares
 
-# class Pawn:
+class Pawn:
+    pass
+
 #   Can move two squares forward on the first move
 #   Can move one square forward on every move but the first
 #   Can attack diagonally
